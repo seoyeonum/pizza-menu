7d1dcb97ba150c869ec5f5cb321fca568c14e206
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-09-01 23:59:30 +0900

    📝 chore: add the component named Pizza
    - added the image folder of pizza's pictures
    - added the css example file
    - learned about components as builing blocks
    - practiced creating and reusing a component named Pizza

--- src/index.js
+++ src/index.js
@@ -1,9 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
+
 function App() {
-  const x = 'jonas';
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+// React 에서 Component 를 구성할 때는...
+// 1. function name 은 대문자로 시작할 것
+// 2. markup (대개는 JSX) 를 반환해야 한다.
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 // React v18
@@ -26,3 +90,11 @@ root.render(
 // → OUTPUT 탭에서 에러 내용을 확인할 수도 있다.
 // 4. 위 모든 게 정상이라면, 작성한 코드에 버그가 존재한다.
 // → 학습 과정에서는 JONAS 의 FINAL 코드와 비교해보기!
+
+// Components as Building Blocks
+// : Component?
+// - React applications are entirely made out of component
+// - Building blocks of user interfaces in React
+// - Piece of UI has its own data, logic, and appearance (how it works and looks)
+// - We build complex UIs by building multiple components and combining them
+// - Components can be reused, nested inside each other, and pass data between them
